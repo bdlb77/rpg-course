f4af99fc4416e2e7fcd35e4705309480e1d1f56b
bdlb77 <bryan.leigh77@gmail.com>
2021-07-07 18:27:56 -0700

    Guard Suspicion

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -2,22 +2,31 @@
 using UnityEngine;
 using RPG.Combat;
 using RPG.Core;
+using RPG.Movement;
 
 namespace RPG.Control
 {
     public class AIController : MonoBehaviour
     {
         [SerializeField] float chaseDistance = 5f;
-
+        [SerializeField] float suspicionTime = 5f; // 3 second!
         Fighter fighter;
         GameObject player;
 
         Health health;
+        Mover mover;
+
+        // Starting position of the guard
+        Vector3 guardPosition;
+        // Guard memory! Infinity since the time initially should be really high!  SO when check and updating. .it will always be lower on Initial LOS of player
+        float timeSinceLastSawPlayer = Mathf.Infinity;
         private void Start()
         {
             fighter = GetComponent<Fighter>();
             health = GetComponent<Health>();
+            mover = GetComponent<Mover>();
             player = GameObject.FindWithTag("Player");
+            guardPosition = transform.position;
 
         }
         private void Update()
@@ -26,13 +35,37 @@ namespace RPG.Control
 
             if (InAttackRangeOfPlayer() && fighter.CanAttack(player))
             {
-                fighter.Attack(player);
+                // reset time since last saw back to 0;
+                timeSinceLastSawPlayer = 0;
+                AttackBehaviour();
+
+            }
+            else if (timeSinceLastSawPlayer < suspicionTime)
+            {
+                SuspicionBehaviour();
 
             }
             else
             {
-                fighter.Cancel();
+                GuardBehaviour();
+
             }
+            timeSinceLastSawPlayer += Time.deltaTime;
+        }
+
+        private void GuardBehaviour()
+        {
+            mover.StartMoveAction(guardPosition);
+        }
+
+        private void SuspicionBehaviour()
+        {
+            GetComponent<ActionScheduler>().CancelCurrentAction();
+        }
+
+        private void AttackBehaviour()
+        {
+            fighter.Attack(player);
         }
 
         // called by United
@@ -40,7 +73,7 @@ namespace RPG.Control
         {
             Gizmos.color = Color.blue;
             Gizmos.DrawWireSphere(transform.position, chaseDistance);
-            
+
         }
 
         private bool InAttackRangeOfPlayer()
