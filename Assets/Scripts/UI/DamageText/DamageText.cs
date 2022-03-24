39cde7a02fdca4f00be1535cfd7bc5f40e93483a
bdlb77 <bryan.leigh77@gmail.com>
2022-03-23 23:06:23 -0700

    Set Damage Text according to our set value in binding

---
+++ Assets/Scripts/UI/DamageText/DamageText.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RPG.UI.DamageText
+{
+    public class DamageText : MonoBehaviour
+    {
+        // ! NOT USED SINCE USING DESTROYER.CS SCRIPT FOR NEWER VERSION OF UNITY
+        [SerializeField] Text damageText = null;
+        public void DestroyText()
+        {
+            Destroy(gameObject);
+        }
+
+        public void SetValue(float amount)
+        {
+            damageText.text = String.Format("{0:0}", amount);
+        }
+    }
+}
