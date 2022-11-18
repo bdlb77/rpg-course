526d36027768ebfb38bcbee69644833ca4c91367
bdlb77 <bryan.leigh77@gmail.com>
2022-11-17 22:43:38 -0800

    Scritable Objects for Dialogue Editor and opening Editor Window

---
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace RPG.Dialogue
+{
+    [CreateAssetMenu(fileName = "New Dialogue", menuName = "Dialogue", order = 0)]
+    public class Dialogue : ScriptableObject
+    {
+        [SerializeField]
+        DialogueNode[] nodes;
+
+    }
+
+}
